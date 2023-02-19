6df0c1098fcc627895e60157ab3fe48d229df9b7
Sirenfal <avosirenfal@gmail.com>
2023-02-19 01:15:49 -0500

    * Initial commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "StashApp organizer";
+  
+  inputs  = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    gomod2nix.url = "github:nix-community/gomod2nix";
+    stashapp = {
+      type = "github";
+      owner = "stashapp";
+      repo = "stash";
+      ref = "v0.19.0";
+      flake = false;
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, gomod2nix, stashapp }:
+    (flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ gomod2nix.overlays.default ];
+          };
+
+        in
+        {
+          packages.default = pkgs.callPackage ./. { };
+          devShells.default = import ./shell.nix { inherit pkgs; };
+        })
+    );
+}
